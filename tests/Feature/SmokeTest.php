c46d87adcfaab9bfa3dcb0d11d1b482de319ffa3
Abrar Ahmad <abrar.dev99@gmail.com>
2025-01-13 11:40:21 +0500

    wip adding tests

---
+++ tests/Feature/SmokeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Livewire\Comments;
+use App\Livewire\Reaction;
+use App\Models\Post;
+
+test('home page returns a successful response', function () {
+    $this->get(route('home'))
+        ->assertStatus(200);
+});
+
+test('show post page returns a successful response', function () {
+    $this->get(route('posts.show', Post::factory()->create()->uuid))
+        ->assertStatus(200);
+});
+
+test('reaction component exists in home page', function () {
+    $this->get(route('home'))
+        ->assertSeeLivewire(Reaction::class);
+});
+
+test('reaction component exists in show post page', function () {
+    $this->get(route('posts.show', Post::factory()->create()->uuid))
+        ->assertSeeLivewire(Reaction::class);
+});
+
+test('comments component exists in show post page', function () {
+    $this->get(route('posts.show', Post::factory()->create()->uuid))
+        ->assertSeeLivewire(Comments\Index::class);
+});
